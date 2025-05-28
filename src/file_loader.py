36dd1e3f6addadfb412ce616f826f71727fb0ee0
Mai Lam <144957302+maisielam@users.noreply.github.com>
2025-05-27 20:29:27 -0400

    Add files via upload

--- src/file_loader.py
+++ src/file_loader.py
@@ -1,49 +1,54 @@
 #%%
+#load PDF files, preprocess text and split into chunks
+
 from typing import Union, List, Literal 
-import glob
-from tqdm import tqdm 
-import multiprocessing
-from langchain_community.document_loaders import PyPDFLoader 
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+import glob 
+from tqdm import tqdm #progress bar to see files have been loaded
+import multiprocessing #run on multiple CPU, speeds up loading PDFs
+from langchain_community.document_loaders import PyPDFLoader #parses PDF files into list of documents
+from langchain_text_splitters import RecursiveCharacterTextSplitter #splits text into smaller chunks
 
-def remove_non_utf8_characters (text):
-    return ''. join (char for char in text if ord(char) < 128)
+def remove_non_utf8_characters (text):  #Removes characters not basic English letters or symbols
+    return ''. join (char for char in text if ord(char) < 128) #basic English characters
 
-def load_pdf (pdf_file):
+def load_pdf (pdf_file):  #Reads PDF using LangChain
     docs = PyPDFLoader (pdf_file, extract_images=True). load ()
     for doc in docs:
-        doc.page_content = remove_non_utf8_characters (doc.page_content)
+        doc.page_content = remove_non_utf8_characters (doc.page_content) #clean each page
     return docs
 
 def get_num_cpu ():
-    return multiprocessing.cpu_count()
+    """Return the number of available CPU cores to set parallel processing"""
+    return multiprocessing.cpu_count() 
 
 
 class BaseLoader:
-    def __init__ (self) -> None:
+    def __init__ (self) -> None: #return None, just store number of CPU
         self.num_processes = get_num_cpu()
-    def __call__(self, files: List[str], **kwargs):
-        pass
+    #make placeholder to be overridden by child classes
+    def __call__(self, files: List[str], **kwargs): #kwargs: collect additional parameters not strict with any parameters
+        pass #kwargs helps future optional parameters can be added without modifying every subclass
 
 
 class PDFLoader(BaseLoader):
-    def __init__(self) -> None:
+    """Loads and cleans PDF documents using multiprocessing."""
+    def __init__(self) -> None: #Inherits from BaseLoader
         super().__init__()
 
-    def __call__(self, pdf_files: List[str], **kwargs):
-        num_processes = min(self.num_processes, kwargs["workers"])
-        with multiprocessing.Pool(processes=num_processes) as pool:
+    def __call__(self, pdf_files: List[str], **kwargs): #loads PDFs with multiprocessing
+        num_processes = min(self.num_processes, kwargs["workers"]) #Uses minimum of actual CPU cores to avoid overload
+        with multiprocessing.Pool(processes=num_processes) as pool: #Creates a pool of processes to load files in parallel
             doc_loaded = []
             total_files = len(pdf_files)
-            with tqdm(total=total_files, desc="Loading PDFs", unit="file") as pbar:
-                for result in pool.imap_unordered(load_pdf, pdf_files):
-                    doc_loaded.extend(result)
-                    pbar.update(1)
+            with tqdm(total=total_files, desc="Loading PDFs", unit="file") as pbar: #show progress bar
+                for result in pool.imap_unordered(load_pdf, pdf_files): #load files in any order (faster)
+                    doc_loaded.extend(result) #merge all results into one list
+                    pbar.update(1) #update progress bar
         return doc_loaded
     
 
 
-class TextSplitter:
+class TextSplitter: #splitting text into smaller parts
     def __init__(self, 
                 separators: List[str] = ['\n\n', '\n', ' ', ''],
                 chunk_size: int = 300,
@@ -55,13 +60,13 @@ class TextSplitter:
             chunk_overlap=chunk_overlap,
         )
 
-    def __call__(self, documents):
+    def __call__(self, documents): #returns a list of smaller chunks
         return self.splitter.split_documents(documents)
 
 
 class Loader:
     def __init__(self,
-                file_type: str = Literal["pdf"],
+                file_type: str = Literal["pdf"], #only pdf supported
                 split_kwargs: dict = {
                     "chunk_size": 300,
                     "chunk_overlap": 0}
@@ -72,20 +77,21 @@ class Loader:
             self.doc_loader = PDFLoader ()
         else :
             raise ValueError("file_type must be pdf")
-        self.doc_spltter = TextSplitter (**split_kwargs)
+        self.doc_spltter = TextSplitter (**split_kwargs) #unpack the dictionary above of split_kwargs
 
-    def load(self, pdf_files: Union[str, List[str]], workers: int = 1):
-        if isinstance(pdf_files, str):
+    def load(self, pdf_files: Union[str, List[str]], workers: int = 1): #Set number of workers for parallel processing
+        """ set workers = 1 to load files sequentially (default) to avoid crash, > 1 to load in parallel """
+        if isinstance(pdf_files, str): #check if pdf_files is a string, then convert to list
             pdf_files = [pdf_files]
-        pdf_files = pdf_files[:5]
-        doc_loaded = self.doc_loader (pdf_files, workers=workers)
-        doc_split = self.doc_spltter (doc_loaded)
+        pdf_files = pdf_files[:5] # Limit to 5 files for testing, remove later
+        doc_loaded = self.doc_loader (pdf_files, workers=workers) #change number of workers if want to load in parallel
+        doc_split = self.doc_spltter (doc_loaded) #split the loaded documents into chunks
         return doc_split
 
     def load_dir (self, dir_path: str, workers: int = 1):
         if self.file_type == "pdf":
-            files = glob.glob (f"{dir_path}/*.pdf")
-            assert len(files) > 0, f"No {self.file_type} files found in { dir_path}"
+            files = glob.glob (f"{dir_path}/*.pdf") #get all pdf files match the pattern
+            assert len(files) > 0, f"No {self.file_type} files found in { dir_path}" #check if files list is not empty
         else:
             raise ValueError ("file_type must be pdf")
         return self.load(files, workers=workers)
